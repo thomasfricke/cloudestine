07f67c778e0047e23c5ad4a6a52f06cf0ba9ead1
Thomas Fricke <thomas@endocode.com>
2014-12-06 20:14:07 +0100

    changed block aligment

--- hashpath.py
+++ hashpath.py
@@ -14,18 +14,20 @@ class HashPath(object):
     def __init__(self,salt,
                  separator='\n',
                  algorithm=hashlib.sha1,
-                 split_num=4):
+                 split_num=4,
+                 block_size=4096):
         self.salt = salt
         self.algorithm = algorithm
         self.split_num = split_num
         self.separator = separator
+        self.block_size=block_size
         
     def hashpath(self,s):
         digest = self.algorithm(s)
         return digest.hexdigest()
     
     def path(self,name,meta='main',block=0):
-        s=self.separator.join( ( self.salt, name, meta, str(block)  ) ) 
+        s=self.separator.join( ( self.salt, name, meta, str( block - block % self.block_size ) ) )
         splitted=HashPath.__split__(  self.hashpath(s) , self.split_num )
         return os.path.sep.join(splitted)
 
