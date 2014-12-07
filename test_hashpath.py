0b86994c4de9fc7a2d4bbaaf6c20a4e871fca200
Thomas Fricke <thomas@endocode.com>
2014-12-07 16:43:15 +0100

    fixed test for new block computation

--- test_hashpath.py
+++ test_hashpath.py
@@ -10,14 +10,14 @@ class HashTestSha1(unittest2.TestCase):
         
     def setUp(self):
         self.hashpath = HashPath("Salt",separator="\n")
-        self.hash_of_Test = hashlib.sha1('Salt\nTest\nmain\n2').hexdigest()
+        self.hash_of_Test = hashlib.sha1('Salt\nTest\nmain\n0').hexdigest()
         
     def test_hashpath_split(self):
         
         self.assertEqual(self.hash_of_Test,''.join(HashPath.__split__(self.hash_of_Test, self.hashpath.split_num)))
         
     def test_hashpath_and_count_parts(self):
-        path= self.hashpath.path('Test',"main",2)
+        path= self.hashpath.path('Test',"main")
         self.assertEqual(len(path), len(self.hash_of_Test)+9)
         self.assertEqual(self.hash_of_Test, string.replace(path, '/', '', 9))
         
@@ -29,14 +29,14 @@ class HashTestMd5(unittest2.TestCase):
         
     def setUp(self):
         self.hashpath = HashPath("Salt",separator="#", algorithm=hashlib.md5)
-        self.hash_of_Test = hashlib.md5('Salt#Test#main#2').hexdigest()
+        self.hash_of_Test = hashlib.md5('Salt#Test#main#0').hexdigest()
         
     def test_hashpath_split(self):
         
         self.assertEqual(self.hash_of_Test,''.join(HashPath.__split__(self.hash_of_Test, self.hashpath.split_num)))
         
     def test_hashpath_and_count_parts(self):
-        path= self.hashpath.path('Test',"main",2)
+        path= self.hashpath.path('Test',"main")
         self.assertEqual(len(path), len(self.hash_of_Test)+7)
         self.assertEqual(self.hash_of_Test, string.replace(path, '/', '', 7))
 
